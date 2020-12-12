ab99c5fb3df4027dfb02d11555c05932ae7e7f40
Ubuntu <atushi10112313@gmail.com>
2020-12-12 23:59:32 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,16 +1,19 @@
-require 'test_helper'
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
 
-class UserMailerTest < ActionMailer::TestCase
-
-  test "account_activation" do
-    user = users(:michael)
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/account_activation
+  def account_activation
+    user = User.first
     user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
-    assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name,               mail.body.encoded
-    assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    UserMailer.account_activation(user)
+  end
+
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
